1bd4179eb9e91b4cbd98789236d95f50c56c4cff
Asadcoder786 <Anonymousustad798@gmail.com>
2025-12-23 19:57:38 +0500

    Random color picker

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
+
+
+let heading = document.querySelector("#heading")
+
+
 function colorChange() {
     let red =Math.floor(Math.random()*256)
     let green =Math.floor(Math.random()*256)
     let blue =Math.floor(Math.random()*256)
-    document.body.style.backgroundColor=
+      let mixture=document.body.style.backgroundColor=
     "rgba("+red+","+green+","+blue+")"
+    heading.innerHTML=`The Color is : ${mixture}`
 }
